1227508e5a89fe7e785a46edc1e677d3414b36ef
mhanif11 <muhammad.hanif.zc11@gmail.com>
2026-01-16 21:04:07 +0700

    Update

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+# LIBRARIES
 from flask import Flask, render_template, request
 import numpy as np
 import pickle
@@ -8,9 +9,9 @@ from tensorflow.keras.preprocessing.sequence import pad_sequences
 
 app = Flask(__name__)
 
-MAX_LEN = 200 # Panjang maksimum input teks
+MAX_LEN = 200
 
-# Path ke file
+# FILE PATH
 MODEL_PATH = 'models/2_projectnlp_model.h5'
 TOKENIZER_PATH = 'models/tokenizer.pkl'
 LE1_PATH = 'models/le_l1.pkl'
@@ -20,46 +21,42 @@ LE3_PATH = 'models/le_l3.pkl'
 print("Loading assets...")
 model = None 
 try:
-    if os.path.exists(MODEL_PATH): #kalau file models/2_projectnlp_model.h5 ada 
-        model = load_model(MODEL_PATH) #panggil method load_model dari keras.models kemudian disimpan di variabel model
-        with open(TOKENIZER_PATH, 'rb') as f: tokenizer = pickle.load(f) #membuka file tokenizer.pkl dalam mode read binary, kemudian memuat isinya menggunakan pickle dan disimpan di variabel tokenizer
-        with open(LE1_PATH, 'rb') as f: le1 = pickle.load(f) #membuka file le_l1.pkl dalam mode read binary, kemudian memuat isinya menggunakan pickle dan disimpan di variabel le1
-        with open(LE2_PATH, 'rb') as f: le2 = pickle.load(f) #membuka file le_l2.pkl dalam mode read binary, kemudian memuat isinya menggunakan pickle dan disimpan di variabel le2
-        with open(LE3_PATH, 'rb') as f: le3 = pickle.load(f) #membuka file le_l3.pkl dalam mode read binary, kemudian memuat isinya menggunakan pickle dan disimpan di variabel le3
+    if os.path.exists(MODEL_PATH):
+        model = load_model(MODEL_PATH)
+        with open(TOKENIZER_PATH, 'rb') as f: tokenizer = pickle.load(f)
+        with open(LE1_PATH, 'rb') as f: le1 = pickle.load(f)
+        with open(LE2_PATH, 'rb') as f: le2 = pickle.load(f)
+        with open(LE3_PATH, 'rb') as f: le3 = pickle.load(f)
         print("Selesai")
     else:
         print("File tidak ditemukan")
 except Exception as e:
     print(f">>> ERROR Loading Model: {e}")
 
-# Fungsi untuk membersihkan teks
+# CLEAN TEXT FUCTION
 def clean_text(text): 
-    text = str(text).lower() #ubah semua huruf jadi kecil
-    text = re.sub(r'[^a-z0-9\s]', '', text) #hapus karakter selain huruf, angka dan spasi
-    return text #mengembalikan teks yang sudah dibersihkan
+    text = str(text).lower()
+    text = re.sub(r'[^a-z0-9\s]', '', text)
+    return text
 
-@app.route('/', methods=['GET', 'POST']) #route untuk halaman utama, menerima metode GET dan POST
-def index(): #fungsi index untuk menangani permintaan ke route '/'
-    prediction = None #set variabel prediction ke None
-    original_text = "" #set variabel original_text ke string kosong
+# MAIN ROUTE
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    prediction = None
+    original_text = ""
     
     if request.method == 'POST': 
-        original_text = request.form['text_input'] #mengambil data dari form input dengan nama 'text_input'
+        original_text = request.form['text_input']
         
-        if model: #cek apakah model sudah dimuat
-            # Preprocess & Predict
+        if model:
             cleaned = clean_text(original_text)
-            seq = tokenizer.texts_to_sequences([cleaned]) #mengubah teks yang sudah dibersihkan menjadi urutan angka menggunakan tokenizer
-            padded = pad_sequences(seq, maxlen=MAX_LEN, padding='post', truncating='post') #memastikan urutan angka memiliki panjang yang konsisten dengan menambahkan padding atau memotongnya sesuai MAX_LEN
+            seq = tokenizer.texts_to_sequences([cleaned])
+            padded = pad_sequences(seq, maxlen=MAX_LEN, padding='post', truncating='post')
             
-            # Predict (Returns list of 3 numpy arrays)
-            preds = model.predict(padded) #melakukan prediksi menggunakan model dengan input padded
+            preds = model.predict(padded)
             
-            # Extract Results
-            # Output model urutannya: [out1, out2, out3]
             res_l1, res_l2, res_l3 = preds[0][0], preds[1][0], preds[2][0]
             
-            # Get Max Probability
             idx1, idx2, idx3 = np.argmax(res_l1), np.argmax(res_l2), np.argmax(res_l3)
             
             prediction = {
@@ -76,4 +73,4 @@ def index(): #fungsi index untuk menangani permintaan ke route '/'
     return render_template('index.html', prediction=prediction, original_text=original_text)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=5001)
--- training_model.py
+++ training_model.py
@@ -1,10 +1,10 @@
+# LIBRARIES
 import pandas as pd
 import numpy as np
 import re
 import pickle
 import os
 
-# Libraries
 from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 from tensorflow.keras.utils import to_categorical
@@ -15,71 +15,69 @@ from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
 from gensim.models import Word2Vec
 
-# --- KONFIGURASI ---
-MAX_LEN = 200 #setiap kalimat dipotong atau klo kurang dikasi padding sampai 200 kata
-EMBEDDING_DIM = 100  #setiap kata diubah jadi deretan 100 angka
-VOCAB_SIZE = 20000 #koskata paling banyak muncul, selebihnya diabaikan
+# KONFIGURASI
+MAX_LEN = 200
+EMBEDDING_DIM = 100
+VOCAB_SIZE = 20000
 EPOCHS = 10 
 BATCH_SIZE = 64 
 
-if not os.path.exists('models'): #buat folder models kalo belum ada
+if not os.path.exists('models'):
     os.makedirs('models')
 
 print("1. MEMUAT DATASET...")
-df = pd.read_csv('dataset/DBP_wiki_data.csv')  #baca dataset dari file CSV
-df = df[['text', 'l1', 'l2', 'l3']].dropna() #ambil kolom text, l1,l2,l3 dan hapus baris kalau ada nilai NaN
+df = pd.read_csv('dataset/DBP_wiki_data.csv')
+df = df[['text', 'l1', 'l2', 'l3']].dropna()
 
-
-df = df.sample(n=100000, random_state=42) #menggambil 50k sample data acak dari dataset 
+df = df.sample(n=100000, random_state=42)
 print(f"Total Data (Sampled): {len(df)}")
 
-#Preprocessing
+# PREPROCESSING
 print("2. CLEANING DATA...")
 
-#method untuk membersikan teks dengan parameter input nya adalah teks
 def clean_text(text):
-    text = str(text).lower() #ubah semua huruf jadi kecil
-    text = re.sub(r'[^a-z0-9\s]', '', text) #hapus karakter selain huruf, angka dan spasi
-    return text #mengembalikan teks yang sudah dibersihkan
+    text = str(text).lower()
+    text = re.sub(r'[^a-z0-9\s]', '', text)
+    return text
 
-df['clean_text'] = df['text'].apply(clean_text) #membuat variabel baru 'clean_text' dengan menerapkan fungsi clean_text pada kolom 'text'
+df['clean_text'] = df['text'].apply(clean_text)
 
-# Label Encoding 
+# LABEL ENCODING 
 print("3. Label Encoder")
 
-#Level 1
-le_l1 = LabelEncoder() #var le_l1 instance object dari LabelEncoder 
+# LEVEL 1
+le_l1 = LabelEncoder()
 y1 = le_l1.fit_transform(df['l1'])
 y1_cat = to_categorical(y1)
 with open('models/le_l1.pkl', 'wb') as f: pickle.dump(le_l1, f)
 
-#Level 2 
-le_l2 = LabelEncoder() #var le_l2 instance object dari LabelEncoder 
+# LEVEL 2 
+le_l2 = LabelEncoder()
 y2 = le_l2.fit_transform(df['l2'])
 y2_cat = to_categorical(y2)
 with open('models/le_l2.pkl', 'wb') as f: pickle.dump(le_l2, f) 
 
-#Level 3
-le_l3 = LabelEncoder() #var le_l3 instance object dari LabelEncoder 
+# LEVEL 3
+le_l3 = LabelEncoder()
 y3 = le_l3.fit_transform(df['l3'])
 y3_cat = to_categorical(y3)
 with open('models/le_l3.pkl', 'wb') as f: pickle.dump(le_l3, f) 
 
-#Tokenization
+# TOKENIZATION
 print("4. Tokenisasi ")
 tokenizer = Tokenizer(num_words=VOCAB_SIZE, oov_token="<OOV>")
-tokenizer.fit_on_texts(df['clean_text']) #mengubah teks bersih menjadi urutan angka
+tokenizer.fit_on_texts(df['clean_text'])
 sequences = tokenizer.texts_to_sequences(df['clean_text'])
 X = pad_sequences(sequences, maxlen=MAX_LEN, padding='post', truncating='post')
 
 with open('models/tokenizer.pkl', 'wb') as f: pickle.dump(tokenizer, f)
 
-# Split Data
+# SPLIT DATA
 X_train, X_test, y1_train, y1_test, y2_train, y2_test, y3_train, y3_test = train_test_split(
     X, y1_cat, y2_cat, y3_cat, test_size=0.2, random_state=42
 )
 
-#Word2Vec 
+# WORD2VEC
 print("5. Training Word2Vec ")
 sentences = [text.split() for text in df['clean_text']]
 w2v_model = Word2Vec(sentences, vector_size=EMBEDDING_DIM, window=5, min_count=2, workers=4) #CBOW
@@ -95,27 +93,27 @@ for word, i in word_index.items():
         else:
             embedding_matrix[i] = np.random.normal(scale=0.6, size=(EMBEDDING_DIM,))
 
-#Model Building
+# BUILD MODEL
 print("6. Membangun Model")
 
 input_layer = Input(shape=(MAX_LEN,))
 
-# Embedding
+# EMBEDDING
 embedding = Embedding(input_dim=num_words, 
                       output_dim=EMBEDDING_DIM, 
                       weights=[embedding_matrix], 
                       trainable=False)(input_layer)
 
-#CNN
+# CNN
 cnn = Conv1D(filters=128, kernel_size=5, activation='relu')(embedding)
 cnn = MaxPooling1D(pool_size=2)(cnn)
 cnn = Dropout(0.2)(cnn)
 
-#LSTM
+# LSTM
 lstm = LSTM(128, return_sequences=False)(cnn)
 lstm = Dropout(0.2)(lstm)
 
-#CABANG OUTPUT
+# CABANG OUTPUT
 # Output 1: Level 1
 dense1 = Dense(64, activation='relu')(lstm)
 out1 = Dense(len(le_l1.classes_), activation='softmax', name='output_l1')(dense1)
@@ -124,7 +122,7 @@ out1 = Dense(len(le_l1.classes_), activation='softmax', name='output_l1')(dense1
 dense2 = Dense(64, activation='relu')(lstm)
 out2 = Dense(len(le_l2.classes_), activation='softmax', name='output_l2')(dense2)
 
-# Output 3: Level 3 (INI YANG TADI HILANG)
+# Output 3: Level 3
 dense3 = Dense(64, activation='relu')(lstm)
 out3 = Dense(len(le_l3.classes_), activation='softmax', name='output_l3')(dense3)
 
@@ -135,7 +133,7 @@ model.compile(loss='categorical_crossentropy', optimizer='adam',
               metrics={'output_l1': 'accuracy', 'output_l2': 'accuracy', 'output_l3': 'accuracy'})
 model.summary()
 
-#Training
+# TRAINING
 print("7. Training...")
 callbacks = [
     EarlyStopping(monitor='val_loss', patience=3, verbose=1),
@@ -151,5 +149,23 @@ history = model.fit(
     batch_size=BATCH_SIZE,
     callbacks=callbacks
 )
+
+# ACC SUMMARY
+print("\n" + "="*55)
+print(f"{'FINAL TRAINING SUMMARY':^55}")
+print("="*55)
+print(f"{'Output Level':<15} | {'Train Acc':<15} | {'Val Acc':<15}")
+print("-" * 55)
+
+for i in range(1, 4):
+    key_acc = f'output_l{i}_accuracy'
+    key_val = f'val_output_l{i}_accuracy'
+    
+    acc = history.history[key_acc][-1] * 100
+    val = history.history[key_val][-1] * 100
+    
+    print(f"{f'Level {i}':<15} | {acc:.2f}%          | {val:.2f}%")
+
+print("="*55 + "\n")
 
 print("Training Selesai.")
